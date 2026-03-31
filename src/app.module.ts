3f9f40309a00a60e5ff79512b77fdd38b678d60a
Arthu085 <arthurghizi22@gmail.com>
2026-03-31 10:44:37 -0300

    chore: add main config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './core/database/typeorm/typeorm.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { throttleConfig } from './core/config/throttle/throttle.config';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [
+    DatabaseModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: throttleConfig.ttl,
+        limit: throttleConfig.limit,
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { corsConfig } from './core/config/network/cors.config';
+import cookieParser from 'cookie-parser';
+import { setupSwagger } from './core/config/swagger/swagger.setup';
+import { envConfig } from './core/config/env/env.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.enableCors(corsConfig);
+  app.use(cookieParser());
+  setupSwagger(app);
+
+  await app.listen(envConfig.PORT);
 }
 bootstrap();
