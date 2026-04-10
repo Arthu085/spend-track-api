99724f5857d7b3586654ca6d2470021872466067
Arthu085 <arthurghizi22@gmail.com>
2026-04-10 10:55:59 -0300

    feat: add ability entity domain

---
+++ src/modules/access-control/domain/entities/ability.entity.ts
@@ -0,0 +1,59 @@
+import { BaseEntity } from 'src/core/domain/entities/base.entity';
+import { ActionEnum } from '../enums/action.enum';
+import { SubjectEnum } from '../enums/subject.enum';
+import { Uuid } from 'src/core/domain/value-objects/uuid.vo';
+import { StatusEnum } from 'src/core/domain/enums/status.enum';
+
+export class AbilityEntity extends BaseEntity {
+  private _action: ActionEnum;
+  private _subject: SubjectEnum;
+
+  private constructor(props: {
+    id?: number;
+    uuid: Uuid;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    status: StatusEnum;
+    action: ActionEnum;
+    subject: SubjectEnum;
+  }) {
+    super(props);
+
+    if (!props.action) {
+      throw new Error('Ação é obrigatória');
+    }
+
+    if (!props.subject) {
+      throw new Error('Sujeito é obrigatório');
+    }
+
+    this._action = props.action;
+    this._subject = props.subject;
+  }
+
+  get action(): ActionEnum {
+    return this._action;
+  }
+
+  get subject(): SubjectEnum {
+    return this._subject;
+  }
+
+  static rehydrate(props: {
+    id: number;
+    uuid: Uuid;
+    createdAt: Date;
+    updatedAt: Date;
+    deletedAt?: Date | null;
+    status: StatusEnum;
+    action: ActionEnum;
+    subject: SubjectEnum;
+  }): AbilityEntity {
+    return new AbilityEntity(props);
+  }
+
+  toKey(): string {
+    return `${this._action}:${this._subject}`;
+  }
+}
